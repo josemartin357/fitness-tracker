d7e74b9350bd4507a5bc8dc350bdbf9f6d5842a5
Martin Bedoya <macowner@macs-MacBook-Pro.local>
2021-12-09 12:52:19 -0600

    Added html routes to display html files inside public folder

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,19 @@
 const path = require("path");
 const router = require("express").Router();
 
+//display home page
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "./public/index.html"));
+});
+
+//display exercise page
+app.get("/exercise", (req, res) => {
+  res.sendFile(path.join(__dirname + "./public/exercise.html"));
+});
+
+//display stats page
+app.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname + "./public/stats.html"));
+});
+
 module.exports = router;
