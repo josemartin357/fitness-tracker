51de7e2fd9e00ddfd5858891ee7346c0911baf71
Martin Bedoya <macowner@macs-MacBook-Pro.local>
2021-12-11 14:29:39 -0600

    deploying

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
