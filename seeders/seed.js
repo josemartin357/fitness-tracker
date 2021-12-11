93a50e06169777b70d7df897254096fdef331bae
Martin Bedoya <macowner@macs-MacBook-Pro.local>
2021-12-11 16:05:54 -0600

    changed heroku to myamazingworkout

--- seeders/seed.js
+++ seeders/seed.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const db = require("../models");
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect("mongodb://localhost/workouts", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
